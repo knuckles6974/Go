09563e86e2e4f5d83e0326b0440e3e0945095fef
knuckles6974 <knuckles6974@gmail.com>
2022-07-23 20:22:43 +0900

    Modify files

--- hello.go
+++ hello.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
+func main(){
+    var a = 133.12341
+	//var b = 3.14
+	//var c = 123456
+	fmt.Printf("%08.2g\n", a)
+	fmt.Printf("%08.1g\n", a)
 
-func main() {
-	fmt.Println("whoo")
 }
